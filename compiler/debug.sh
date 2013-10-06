2c4a66593ddc0eee5e41083113f527b45803e582
Zack Newman <znewman01@gmail.com>
2013-10-06 16:34:30 -0400

    Add script to see the output of parsing

---
+++ compiler/debug.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# This script will parse and scan STDIN into an AST, which it then prints.
+# 
+# Usage:
+#   echo '3 + 3' | $0
+# or
+#   cat example_file.vec | $0
+#
+# It contains many kludges.
+
+scons > /dev/null
+
+PARSE='
+open Ast;;\n
+\n
+let lexbuf = Lexing.from_channel stdin in\n
+Parser.expr Scanner.token lexbuf;;'
+(echo -e $PARSE; cat -) | ocaml scanner.cmo parser.cmo | tail -n +3 | head -n -1
