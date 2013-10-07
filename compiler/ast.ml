d28ea57ed107d45eecf842ada6f8271b8c695831
Howard Mao <zhehao.mao@gmail.com>
2013-10-06 23:13:30 -0400

    add statement AST type

--- compiler/ast.ml
+++ compiler/ast.ml
@@ -8,3 +8,11 @@ type expr =
   | Assign of string * expr
   | IntLit of int
   | Ident of string ;;
+
+type statement =
+    CompoundStatement of statement list
+  | Expression of expr
+  | AssigningDecl of string * expr
+  | PrimitiveDecl of string * string
+  | ArrayDecl of string * string * expr
+  | EmptyStatement
