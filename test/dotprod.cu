93611b6464fc0d171706f04417041290a8b4d3b0
Howard Mao <zhehao.mao@gmail.com>
2013-10-27 21:27:50 -0400

    rewrite dotprod and vecmult using libvector

--- test/dotprod.cu
+++ test/dotprod.cu
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "vector_utils.hpp"
+#include "libvector.hpp"
 
 __global__
 void dotprod_kernel(int *dotprod, int *x, int *y, size_t n)
@@ -10,7 +10,7 @@ void dotprod_kernel(int *dotprod, int *x, int *y, size_t n)
 
     if (i < n)
         dotprod[i] = x[i] * y[i];
-    
+
     for (s = 1; s < n; s *= 2) {
         if (i % (2 * s) == 0 && i + s < n)
             dotprod[i] += dotprod[i + s];
@@ -20,36 +20,19 @@ void dotprod_kernel(int *dotprod, int *x, int *y, size_t n)
 
 int main(void)
 {
-    int x[4] = {1, 2, 3, 4};
-    int y[4] = {3, 5, 7, 9};
-    int dp;
+    VectorArray<int> x = array_init<int>(4, 1, 2, 3, 4);
+    VectorArray<int> y = array_init<int>(4, 3, 5, 7, 9);
+    VectorArray<int> dp(1, 4);
 
-    int *d_x, *d_y, *d_dp;
-    
-    cudaError_t err;
-    
-    err = cudaMalloc(&d_x, sizeof(x));
-    checkError(err);
-    err = cudaMalloc(&d_y, sizeof(y));
-    checkError(err);
-    err = cudaMalloc(&d_dp, sizeof(x));
-    checkError(err);
+    x.copyToDevice();
+    y.copyToDevice();
 
-    err = cudaMemcpy(d_x, x, sizeof(x), cudaMemcpyHostToDevice);
-    checkError(err);
-    err = cudaMemcpy(d_y, y, sizeof(y), cudaMemcpyHostToDevice);
-    checkError(err);
-
-    dotprod_kernel<<<1, 4>>>(d_dp, d_x, d_y, 4);    
+    dotprod_kernel<<<1, 4>>>(dp.devPtr(), x.devPtr(), y.devPtr(), 4);
     cudaDeviceSynchronize();
     checkError(cudaGetLastError());
 
-    cudaMemcpy(&dp, d_dp, sizeof(dp), cudaMemcpyDeviceToHost);
-    printf("Dot product: %d\n", dp);
-
-    cudaFree(d_x);
-    cudaFree(d_y);
-    cudaFree(d_dp);
+    dp.copyFromDevice();
+    printf("Dot product: %d\n", dp.elem(0));
 
     return 0;
 }
--- test/vecmult.cu
+++ test/vecmult.cu
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "vector_utils.hpp"
+#include <libvector.hpp>
 
 #define VEC_SIZE 5000
 #define BLOCK_SIZE 512
@@ -17,55 +17,36 @@ void vec_mult(long *a, long *b, long *c, size_t n)
 
 int main(void)
 {
-    long a[VEC_SIZE], b[VEC_SIZE], c[VEC_SIZE];
-    long *d_a, *d_b, *d_c;
+    VectorArray<long> a(1, VEC_SIZE), b(1, VEC_SIZE), c(1, VEC_SIZE);
     int i;
     cudaError_t err;
     size_t numblocks, numthreads;
 
-    d_a = d_b = d_c = NULL;
-
     srandom(time(0));
 
     for (i = 0; i < VEC_SIZE; i++) {
-        a[i] = random();
-        b[i] = random();
+        a.elem(i) = random();
+        b.elem(i) = random();
     }
 
-    err = cudaMalloc(&d_a, sizeof(a));
-    if (err != cudaSuccess)
-        goto cleanup;
-    err = cudaMalloc(&d_b, sizeof(b));
-    if (err != cudaSuccess)
-        goto cleanup;
-    err = cudaMalloc(&d_c, sizeof(c));
-    if (err != cudaSuccess)
-        goto cleanup;
-
-    err = cudaMemcpy(d_a, a, sizeof(a), cudaMemcpyHostToDevice);
-    if (err != cudaSuccess)
-        goto cleanup;
-    err = cudaMemcpy(d_b, b, sizeof(b), cudaMemcpyHostToDevice);
-    if (err != cudaSuccess)
-        goto cleanup;
+    a.copyToDevice();
+    b.copyToDevice();
 
     numblocks = ceil_div(VEC_SIZE, BLOCK_SIZE);
     numthreads = BLOCK_SIZE;
-    vec_mult<<<numblocks, numthreads>>>(d_a, d_b, d_c, VEC_SIZE);
+    vec_mult<<<numblocks, numthreads>>>(a.devPtr(), b.devPtr(), c.devPtr(), VEC_SIZE);
 
     err = cudaDeviceSynchronize();
     if (err != cudaSuccess)
         goto cleanup;
 
-    err = cudaMemcpy(c, d_c, sizeof(c), cudaMemcpyDeviceToHost);
-    if (err != cudaSuccess)
-        goto cleanup;
+    c.copyFromDevice();
 
     err = cudaSuccess;
 
     for (i = 0; i < VEC_SIZE; i++) {
-        if (c[i] != a[i] * b[i]) {
-            fprintf(stderr, "Mismatched number %ld\n", c[i]);
+        if (c.elem(i) != a.elem(i) * b.elem(i)) {
+            fprintf(stderr, "Mismatched number %ld\n", c.elem(i));
             goto cleanup;
         }
     }
@@ -74,11 +55,5 @@ int main(void)
 cleanup:
     if (err != cudaSuccess && err > 0)
         fprintf(stderr, "%s\n", cudaGetErrorString(err));
-    if (d_a != NULL)
-        cudaFree(d_a);
-    if (d_b != NULL)
-        cudaFree(d_b);
-    if (d_c != NULL)
-        cudaFree(d_c);
     return err;
 }
