e0e8eb65cdefec17ee8b898864339e6b9caf655c
Howard Mao <zhehao.mao@gmail.com>
2013-10-27 21:28:16 -0400

    rewrite mandelbrot using libvector and take away random colorMap generation

--- test/mandelbrot.cu
+++ test/mandelbrot.cu
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "vector_utils.hpp"
+#include <libvector.hpp>
 
 #define MAX_ITER 1000
 #define LEFT_MIN -2.5
@@ -43,10 +43,11 @@ void mandelbrot(uchar3 *colors, uchar3 *colorMap,
     colors[i] = colorMap[iter];
 }
 
-int write_ppm(FILE *f, uchar3 *colors,
+int write_ppm(FILE *f, VectorArray<uchar3> &colors,
         unsigned int width, unsigned int height)
 {
     size_t x, y, i;
+    uchar3 pixel;
 
     fprintf(f, "P3\n");
     fprintf(f, "%u %u\n", width, height);
@@ -55,7 +56,8 @@ int write_ppm(FILE *f, uchar3 *colors,
     for (y = 0; y < height; y++) {
         for (x = 0; x < width; x++) {
             i = y * width + x;
-            fprintf(f, "%d %d %d ", colors[i].x, colors[i].y, colors[i].z);
+            pixel = colors.elem(i);
+            fprintf(f, "%d %d %d ", pixel.x, pixel.y, pixel.z);
         }
         fprintf(f, "\n");
     }
@@ -65,13 +67,11 @@ int write_ppm(FILE *f, uchar3 *colors,
 int main(int argc, char *argv[])
 {
     float left, right, top, bottom;
-    uchar3 colors[IMG_WIDTH * IMG_HEIGHT], colorMap[MAX_ITER + 1];
-    uchar3 *d_colors = NULL, *d_colorMap;
-    cudaError_t err;
+    VectorArray<uchar3> colors(1, IMG_WIDTH * IMG_HEIGHT);
+    VectorArray<uchar3> colorMap(1, MAX_ITER + 1);
     dim3 grid_dim(IMG_WIDTH / BLOCK_WIDTH, IMG_HEIGHT / BLOCK_HEIGHT, 1);
     dim3 block_dim(BLOCK_WIDTH, BLOCK_HEIGHT, 1);
     int i;
-    long ran;
 
     left = (argc > 1) ? atof(argv[1]) : -2.0;
     right = (argc > 2) ? atof(argv[2]) : 1.0;
@@ -84,43 +84,27 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    srandom(0);
-
     for (i = 0; i < MAX_ITER; i++) {
-        ran = random();
-        colorMap[i].x = (ran >> 16) & 0xff;
-        colorMap[i].y = (ran >> 8) & 0xff;
-        colorMap[i].z = ran & 0xff;
+        colorMap.elem(i).x = i & 0xff;
+        colorMap.elem(i).y = i & 0xff;
+        colorMap.elem(i).z = i & 0xff;
     }
 
-    colorMap[MAX_ITER].x = 0;
-    colorMap[MAX_ITER].y = 0;
-    colorMap[MAX_ITER].z = 0;
+    colorMap.elem(MAX_ITER).x = 0;
+    colorMap.elem(MAX_ITER).y = 0;
+    colorMap.elem(MAX_ITER).z = 0;
 
-    err = cudaMalloc(&d_colors, sizeof(colors));
-    checkError(err);
+    colorMap.copyToDevice();
 
-    err = cudaMemset(d_colors, 0, sizeof(colors));
-
-    err = cudaMalloc(&d_colorMap, sizeof(colorMap));
-    checkError(err);
-
-    err = cudaMemcpy(d_colorMap, colorMap, sizeof(colorMap), cudaMemcpyHostToDevice);
-    checkError(err);
-
-    mandelbrot<<<grid_dim, block_dim>>>(d_colors, d_colorMap,
+    mandelbrot<<<grid_dim, block_dim>>>(colors.devPtr(), colorMap.devPtr(),
             left, right, top, bottom);
     cudaDeviceSynchronize();
     checkError(cudaGetLastError());
 
-    err = cudaMemcpy(colors, d_colors, sizeof(colors), cudaMemcpyDeviceToHost);
-    checkError(err);
+    colors.copyFromDevice();
 
     if (write_ppm(stdout, colors, IMG_WIDTH, IMG_HEIGHT))
         exit(EXIT_FAILURE);
-
-    cudaFree(d_colors);
-    cudaFree(d_colorMap);
 
     return 0;
 }
