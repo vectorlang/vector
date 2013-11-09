e849a53d96ee41388ae0485678254dce5615f971
Howard Mao <zhehao.mao@gmail.com>
2013-11-09 12:15:00 -0500

    use vecmult to test memory handling

--- test/vecmult.cu
+++ test/vecmult.cu
@@ -4,10 +4,9 @@
 #include <libvector.hpp>
 
 #define VEC_SIZE 5000
-#define BLOCK_SIZE 512
 
 __global__
-void vec_mult(long *a, long *b, long *c, size_t n)
+void vec_mult_kernel(long *a, long *b, long *c, size_t n)
 {
     size_t i = threadIdx.x + blockDim.x * blockIdx.x;
 
@@ -15,12 +14,31 @@ void vec_mult(long *a, long *b, long *c, size_t n)
         c[i] = a[i] * b[i];
 }
 
+VectorArray<long> vec_mult(VectorArray<long> &a, VectorArray<long> &b)
+{
+    VectorArray<long> c(1, a.size());
+    size_t numblocks, numthreads;
+
+    a.copyToDevice();
+    b.copyToDevice();
+
+    numblocks = ceil_div(c.size(), BLOCK_SIZE);
+    numthreads = BLOCK_SIZE;
+    vec_mult_kernel<<<numblocks, numthreads>>>(a.devPtr(), b.devPtr(),
+            c.devPtr(), c.size());
+
+    cudaDeviceSynchronize();
+    checkError(cudaGetLastError());
+
+    c.copyFromDevice();
+
+    return c;
+}
+
 int main(void)
 {
-    VectorArray<long> a(1, VEC_SIZE), b(1, VEC_SIZE), c(1, VEC_SIZE);
+    VectorArray<long> a(1, VEC_SIZE), b(1, VEC_SIZE);
     int i;
-    cudaError_t err;
-    size_t numblocks, numthreads;
 
     srandom(time(0));
 
@@ -32,28 +50,15 @@ int main(void)
     a.copyToDevice();
     b.copyToDevice();
 
-    numblocks = ceil_div(VEC_SIZE, BLOCK_SIZE);
-    numthreads = BLOCK_SIZE;
-    vec_mult<<<numblocks, numthreads>>>(a.devPtr(), b.devPtr(), c.devPtr(), VEC_SIZE);
-
-    err = cudaDeviceSynchronize();
-    if (err != cudaSuccess)
-        goto cleanup;
-
-    c.copyFromDevice();
-
-    err = cudaSuccess;
+    VectorArray<long> c = vec_mult(a, b);
 
     for (i = 0; i < VEC_SIZE; i++) {
         if (c.elem(i) != a.elem(i) * b.elem(i)) {
             fprintf(stderr, "Mismatched number %ld\n", c.elem(i));
-            goto cleanup;
+            return -1;
         }
     }
     printf("Computation was correct\n");
 
-cleanup:
-    if (err != cudaSuccess && err > 0)
-        fprintf(stderr, "%s\n", cudaGetErrorString(err));
-    return err;
+    return 0;
 }
