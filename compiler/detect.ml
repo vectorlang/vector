eea34b6fb35342aa0c22bdf6919fe680ad2062dc
Jonathan Yu <jy2432@columbia.edu>
2013-12-05 10:53:14 -0500

    Remove duplicate inputs and outputs

--- compiler/detect.ml
+++ compiler/detect.ml
@@ -3,12 +3,22 @@ open Ast
 
 exception Not_allowed_on_gpu
 
+module IdentSet = Set.Make(
+  struct
+    let compare = (
+      let ident_to_str = function Ident(s) -> s
+      in (fun i1 i2 -> Pervasives.compare (ident_to_str i1) (ident_to_str i2))
+    )
+    type t = ident
+  end
+);;
+
 let combine_detect_tuples tuplist =
     let combine_pair tup1 tup2 =
         let (in1, out1) = tup1 and
             (in2, out2) = tup2 in
-        (in1 @ in2, out1 @ out2) in
-    List.fold_left combine_pair ([], []) tuplist;;
+        (IdentSet.union in1 in2, IdentSet.union out1 out2) in
+    List.fold_left combine_pair (IdentSet.empty, IdentSet.empty) tuplist;;
 
 let rec detect_statement stmt env =
     match stmt with
@@ -41,7 +51,7 @@ and detect_expr expr env =
       | FunctionCall(ident, elist) ->
             detect_expr_list elist env
       | Lval(lvalue) -> detect_lvalue lvalue true false env
-      | _ -> ([], [])
+      | _ -> (IdentSet.empty, IdentSet.empty)
 and detect_expr_list elist env =
     let tuplist = List.map (fun expr -> detect_expr expr env) elist in
     combine_detect_tuples tuplist
@@ -53,25 +63,25 @@ and detect_lvalue lvalue ins outs env =
       | Variable(ident) ->
             if var_in_scope ident env then
                 if outs then
-                    ([], [])
+                    (IdentSet.empty, IdentSet.empty)
                 else if ins then
-                    ([ident], [])
-                else ([], [])
-            else ([], [])
+                    (IdentSet.singleton ident, IdentSet.empty)
+                else (IdentSet.empty, IdentSet.empty)
+            else (IdentSet.empty, IdentSet.empty)
       | ArrayElem(ident, indices) ->
             let (indins, indouts) as indtups =
                 detect_expr_list indices env in
             if var_in_scope ident env then
-                ((if ins then ident :: indins else indins),
-                 (if outs then ident :: indouts else indouts))
+                ((if ins then IdentSet.add ident indins else indins),
+                 (if outs then IdentSet.add ident indouts else indouts))
             else indtups
       | ComplexAccess(expr, _) ->
             (match expr with
               | Lval(Variable(ident)) ->
                     if var_in_scope ident env then
-                        ((if ins then [ident] else []),
-                         (if outs then [ident] else []))
-                    else ([], [])
+                        ((if ins then IdentSet.singleton ident else IdentSet.empty),
+                         (if outs then IdentSet.singleton ident else IdentSet.empty))
+                    else (IdentSet.empty, IdentSet.empty)
               | _ -> detect_expr expr env)
 and detect_iter iter env =
     match iter with
@@ -87,3 +97,6 @@ and detect_range range env =
     combine_detect_tuples
         [detect_expr expr1 env; detect_expr expr2 env; detect_expr expr3 env]
 
+let detect stmt env =
+    let gpu_inputs, gpu_outputs = detect_statement stmt env in
+    (IdentSet.elements gpu_inputs, IdentSet.elements gpu_outputs)
