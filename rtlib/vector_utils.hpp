9fec23d5bf21cd28656d64122431fca12ca77f6d
Howard Mao <zhehao.mao@gmail.com>
2013-12-15 13:39:39 -0500

    add a time macro

--- rtlib/vector_utils.hpp
+++ rtlib/vector_utils.hpp
@@ -1,6 +1,8 @@
 #ifndef __VECTOR_UTILS_H__
 #define __VECTOR_UTILS_H__
 
+#include <sys/time.h>
+
 static inline void _check(cudaError_t err, const char *file, int line)
 {
     if (err != cudaSuccess) {
@@ -8,6 +10,15 @@ static inline void _check(cudaError_t err, const char *file, int line)
         fprintf(stderr, "%s\n", cudaGetErrorString(err));
         exit(err);
     }
+}
+
+static inline double get_time(void)
+{
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+
+	return (double) tv.tv_sec + ((double) tv.tv_usec) / 1000000.0;
 }
 
 #ifndef BLOCK_SIZE
